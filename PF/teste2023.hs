be03a69329ebff9231cb743b7d35dbb3084087c6
JTN210 <joaoneiva27@gmail.com>
2024-12-10 15:48:38 +0000

    ss

--- PF/teste2023.hs
+++ PF/teste2023.hs
@@ -7,3 +7,11 @@ uniaoMSet mset1 [] = mset1
 uniaoMSet ((x,xs):t) ((y,ys):z) | x == y = (x,xs + ys) : uniaoMSet t z
                                 | x != y = uniaoMSet (x,xs) z ++ uniaoMSet (y,ys) t 
                     
+
+func :: Float -> [(Float,Float)] -> [Float]
+func x y = aux [] x y
+
+aux :: [Float] -> Float -> [(Float,Float)] -> [Float]
+aux n x [] = n 
+aux n x ((a,b):t) | x > a = aux n x t 
+                  | otherwise = aux (n:b) x t 
