b873244363b37eb089abd9bdefd4a997184fb269
JTN210 <joaoneiva27@gmail.com>
2025-02-06 23:18:18 +0000

    PI ficha

---
+++ PI/ficha1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/*int main() {
+for (int i = 0; i <6; i++)
+   {
+    for (int j = 0; j < 6; j++)
+    {putchar('*');
+    
+    }
+    putchar('\n');
+   } 
+
+return 0;
+
+
+}
+*/
+/// 2
+
+int mains(){
+
+   for(int i = 0; i < 6; i++)
+   {
+      for(int j = 0; j < 6; j++)
+      {
+         putchar('# _');
+      }
+
+      putchar('\n');
+
+   }
+   return 0;
+
+
+
+
+
+}
--- PI/main.c
+++ PI/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, WSL!\n");
+   int N = 5;
+    for (int i = 0; i <= N; i++)
+    {
+    printf("Bota Calor\n");
+    
+    }
     return 0;
 }
