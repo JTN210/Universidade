1c10b8494f441cf32c65cb3026f777582170df86
JTN210 <joaoneiva27@gmail.com>
2025-02-07 18:02:05 +0000

    exercicio 3

--- PI/ficha1.c
+++ PI/ficha1.c
@@ -15,14 +15,24 @@ return 0;
 }
 */
 /// 2
+ /// feito a ladrão 
+/*
+int main(){
 
-int mains(){
-
-   for(int i = 0; i < 6; i++)
+   for(int i = 0; i < 5; i++)
    {
-      for(int j = 0; j < 6; j++)
+     
+      for(int j = 0; j < 1; j++)
       {
-         putchar('# _');
+         if( i % 2 == 0 )
+         {
+            printf("#_#_#");
+         }
+         else 
+         {
+            printf("_#_#_");
+         }
+         
       }
 
       putchar('\n');
@@ -30,8 +40,50 @@ int mains(){
    }
    return 0;
 
+}
+
+/// feito direito
+
+int main(){
+      int n = 5;
+   for(int i = 0; i < n; i++)
+   {
+     
+      for(int j = 0; j < n; j++)
+      {
+         if( (j+i) % 2 == 0 )
+         {
+            putchar('#');
+         }
+         else 
+         {
+            putchar('_');
+         }
+         
+      }
+
+      putchar('\n');
+
+   }
+   return 0;
+
+}
 
 
+*/
+/// 3
 
+int main(){
+ int i,j,n = 5;
+for(i = 0; i < n; i++)
+   {
+      for(j = 0;j < i +1; j++)
+      {
+         putchar('#');
+
+      }
+
+      putchar('\n');
+   }
 
 }
