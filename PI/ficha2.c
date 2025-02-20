a05899f270cd3e757f94d8d87f2a9811abcf631f
JTN210 <joaoneiva27@gmail.com>
2025-02-20 16:31:49 +0000

    pi

--- PI/ficha2.c
+++ PI/ficha2.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 
-
-
-int main(){
-
+int main()
+{
 int i,n= 2,m = 5,k = 0;
 
     for(i= 0; i <= n;i++)
     {
-        k += m
+        k += m;
     }
-
-    return k;
+    printf("%d \n", k);
+    return 0;
 }
