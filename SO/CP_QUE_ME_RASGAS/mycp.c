547fcbbfbbba6109a4ce11597034cc3dc6ff928e
JTN210 <joaoneiva27@gmail.com>
2025-02-27 15:55:36 +0000

    cp

--- SO/CP_QUE_ME_RASGAS/mycp.c
+++ SO/CP_QUE_ME_RASGAS/mycp.c
@@ -5,23 +5,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+#define SIZE 1024
 int main(int argc, char *argv[])
-{
+{   
 
     int src = open(argv[1], O_RDONLY);
     int dest = open(argv[2], O_CREAT | O_WRONLY, 0666);
+    ssize_t n_read = 0;
+    char buffer[SIZE];
 
-    off_t pos = lseek(dest, 0, SEEK_END);
-    int buffer_size = atoi(argv[3]);
 
-    ssize_t bytes_read = 0;
-    char *buffer = (char *)malloc(sizeof(char) * buffer_size);
-    while ((bytes_read = read(src, buffer, buffer_size)) > 0)
-    {
-        write(dest, buffer, bytes_read);
-    }
 
-    free(buffer);
+
+    while((n_read = read(src, &buffer, SIZE)) != 0)
+		write(dest, &buffer, n_read);
+
+
     close(src);
     close(dest);
 
