c14bb22abd9fa8d70501362102c0e712c6c90e27
JTN210 <joaoneiva27@gmail.com>
2025-03-10 23:04:49 +0000

    ss

--- PI/ficha4.c
+++ PI/ficha4.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
-
-int tamanho (char *s){
-    int i =0;
-    while (s[i]!= '\0')
+#include <string.h>
+int tamanho(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
     {
         i++;
     }
     return i;
 }
-int contaVogais(char *s){
+int contaVogais(char *s)
+{
     int num = 0;
     while (*s != '\0')
     {
@@ -18,92 +19,88 @@ int contaVogais(char *s){
             *s == 'i' || *s == 'I' ||
             *s == 'o' || *s == 'O' ||
             *s == 'u' || *s == 'U')
-            {
-                num++;
-            }
+        {
+            num++;
+        }
         s++;
-    }    
+    }
     return num;
 }
 
- // *a, "o conteudo do endereço de memoria a"
- // quando existe \0 significa que a string acabou
- int  SerVogal(char s){
+// *a, "o conteudo do endereço de memoria a"
+// quando existe \0 significa que a string acabou
+int SerVogal(char s)
+{
     if (s == 'a' || s == 'A' ||
         s == 'e' || s == 'E' ||
         s == 'i' || s == 'I' ||
         s == 'o' || s == 'O' ||
         s == 'u' || s == 'U')
-        {
-
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
- }
-int retiraVogaisRep(char *s){
-    //int t = tamanho(s);
-    char auxiliar[400000];
-    int inicial,novo,i;
-
-    for (int i = 0; s[i] != '\0'; i++)
     {
-        if (SerVogal(s[i]) != 1)
-        {
-            auxiliar[i] = s[i];
-            inicial++;
+
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int retiraVogaisRep(char *s) {
+    int t = strlen(s);
+    char auxiliar[t + 1]; // +1 para o '\0'
+    int novo = 0;
+    int removidas = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (SerVogal(s[i])) {
+            // Se for vogal e diferente da vogal anterior, copia
+            if (i == 0 || s[i] != s[i - 1]) {
+                auxiliar[novo] = s[i];
+                novo++;
+            } else {
+                removidas++; // vogal repetida, não copia
+            }
+        } else {
+            // Se não for vogal, copia
+            auxiliar[novo] = s[i];
             novo++;
         }
-        else
-        {
-            if(s[i] != s[i+1])
-            {
-               auxiliar[i] = s[i];
-                i++;
-                inicial++;
-                novo++;
-            }
-            else
-            {
-                inicial++;
-            }
-        }     
-    } 
-    int m;
-    for ( m = 0; m < novo; m++)
-    {
-        s[m] = auxiliar[m];
     }
-    s[m] = '\0';
-     
-    return inicial-novo;
+
+    auxiliar[novo] = '\0'; // terminar a string
+
+    // Copiar de volta para s
+    strcpy(s, auxiliar);
+
+    return removidas;
 }
+
 
 int retiraVogaisRep2(char *s){
+    int removidas = 0, novo =0, atual = 0;
 
-    for (int i = 0; s[i] != '\0'; i++)
+    while (s[atual] != '\0')
     {
-        if (SerVogal(s[i]) == 1)
+        if (SerVogal(s[atual]) && s[atual] == s[atual+1])
         {
-            if(s[i] == s[i+1])
-            {
-                s[i] = s[i+1];
-            }
-            else
-            {
-                s[i] = s[i];
-            }
-
-        }     
-    } 
-    return *s;
+            atual++;
+            removidas++;
+        }
+        else
+        {
+            s[novo]= s[atual];
+            atual++;
+            novo++;
+        }
+    }    
+    s[novo] = '\0'; 
+    return removidas;
 }
 
-int main(){
-    char c[] = "proograamaacao";
-    printf("%d\n",retiraVogaisRep(c));
-    printf("%s\n",c);
+int main()
+{
+    char c[] = "Estaa e umaa string coom duuuuplicadoos";
+    printf("%d\n", retiraVogaisRep2(c));
+    printf("%s\n", c);
     return 0;
 }
