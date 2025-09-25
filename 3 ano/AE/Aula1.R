1da0273d49d8f0cfc4807063f745668ad2adc491
JTN210 <joaoneiva27@gmail.com>
2025-09-25 11:13:52 +0100

    NECC

---
+++ 3 ano/AE/Aula1.R
@@ -0,0 +1,12 @@
+a = 4
+b = 5 
+a+b
+c(1,2,3,a,b)
+matrix(1:9,nrow = 3, ncol = 3)
+ 
+dados <- data.frame(nome = c("Ana", "Bruno","Marco","Tomas"), Signo=c("Aquario","peixes","gemeos","carneiro"),cidade=c("Vieira","Braga","Famalicão","Trofa"), cidade=c(1.84,1.72,1.80,1.80))
+dados
+str(dados)
+summary(dados)
+dados[2:4,]
+
